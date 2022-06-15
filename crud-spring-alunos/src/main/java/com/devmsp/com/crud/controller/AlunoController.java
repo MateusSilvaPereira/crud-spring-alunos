63b58b70c3094af9b973c0ac881795e31b38a0f9
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2022-06-15 18:40:55 -0400

    front com html e bootstrap e thymeleaf

--- crud-spring-alunos/src/main/java/com/devmsp/com/crud/controller/AlunoController.java
+++ crud-spring-alunos/src/main/java/com/devmsp/com/crud/controller/AlunoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.devmsp.com.crud.entity.Aluno;
 import com.devmsp.com.crud.service.AlunoService;
